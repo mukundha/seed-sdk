152a02e8c3970db8efa7f6aa7d197cb1ed2637dd
mukundha@apigee.com <mukundha@apigee.com>
2016-08-29 13:20:43 -0700

    seed test

---
+++ seed-browser.js
@@ -0,0 +1,10 @@
+function getTestData(org,env,sample,cb){
+	var ql = "select * where org='" + org + "' and env='" + env + "' and sample='" + sample + "'"
+	var baseurl = 'http://amer-demo1-test.apigee.net/seed'
+
+	jQuery.ajax({
+		url:baseurl+'/testdatasets?ql=' + ql,
+	}).done(function(data){
+		cb(data)
+	})
+}
