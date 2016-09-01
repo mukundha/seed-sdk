f473a7e86e0e199f327e7fed14065f02321e9bf8
mukundha@apigee.com <mukundha@apigee.com>
2016-08-31 19:50:47 -0700

    fix https

--- seed-browser.js
+++ seed-browser.js
@@ -1,6 +1,6 @@
 function getTestData(org,env,sample,cb){
 	var ql = "select * where org='" + org + "' and env='" + env + "' and sample='" + sample + "'"
-	var baseurl = 'http://amer-demo1-test.apigee.net/seed'
+	var baseurl = 'https://amer-demo1-test.apigee.net/seed'
 
 	jQuery.ajax({
 		url:baseurl+'/testdatasets?ql=' + ql,
