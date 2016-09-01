d7da98ae479527c9947f80bdda53c087e730afdd
mukundha@apigee.com <mukundha@apigee.com>
2016-08-31 19:32:19 -0700

    add q

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var request = require('request')
+var q = require('q')
 var baseurl = 'http://amer-demo1-test.apigee.net/seed'
 
 //TODO: add auth, externalize API, add doc
@@ -8,11 +9,14 @@ function sdk(){
 module.exports = sdk
 
 sdk.storeTestData = function(data){
+	var d = q.defer()
 	request({
 		uri: baseurl+ '/testdatasets',
 		body: data,
 		json: true
 	}, function(error, response ,body){
-		console.log(body)
+		if(error) q.reject(error)
+		else q.resolve(body)
 	})
+	return d.promise;
 }
