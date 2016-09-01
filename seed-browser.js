8c998818bfc4144cedfc00db65174f196e5c2794
mukundha@apigee.com <mukundha@apigee.com>
2016-08-31 20:17:42 -0700

    fix params

--- seed-browser.js
+++ seed-browser.js
@@ -1,5 +1,5 @@
 function getTestData(org,env,sample,cb){
-	var ql = "select * where org='" + org + "' and env='" + env + "' and sample='" + sample + "'"
+	var ql = "select * where organization='" + org + "' and environment='" + env + "' and sample='" + sample + "'"
 	var baseurl = 'https://amer-demo1-test.apigee.net/seed'
 
 	jQuery.ajax({
